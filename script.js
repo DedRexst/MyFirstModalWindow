0474a1b5fe37ab2827f4a6653a83990c2968a1b8
DedRexst <hlynpavlo@gmail.com>
2023-07-13 01:12:56 +0300

    Just little code improvement

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ const overlay = document.querySelector(".overlay");
 const btnCloseMedal = document.querySelector(".close-modal");
 const btnsOpenMedal = document.querySelectorAll(".show-modal");
 const hidden = document.querySelector(".hidden");
+
+const CloseMedal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
 for (let i = 0; i < btnsOpenMedal.length; i++) {
     console.log(btnsOpenMedal[i]);
     btnsOpenMedal[i].addEventListener("click", function () {
-        // hidden.style.display = "initial";
         modal.classList.remove("hidden");
         overlay.classList.remove("hidden");
     });
 }
-btnCloseMedal.addEventListener("click", function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-
-    // hidden.style.display = "none";
-});
+btnCloseMedal.addEventListener("click", CloseMedal);
+overlay.addEventListener("click", CloseMedal);
