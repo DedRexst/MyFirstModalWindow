4874875b475d54e71e2e1905c8f417d012cdd365
DedRexst <hlynpavlo@gmail.com>
2023-07-13 03:30:54 +0300

    Dry code

--- script.js
+++ script.js
@@ -6,17 +6,18 @@ const btnCloseMedal = document.querySelector(".close-modal");
 const btnsOpenMedal = document.querySelectorAll(".show-modal");
 const hidden = document.querySelector(".hidden");
 
-const CloseMedal = function () {
+const closeMedal = function () {
     modal.classList.add("hidden");
     overlay.classList.add("hidden");
+};
+const openMedal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
 };
 
 for (let i = 0; i < btnsOpenMedal.length; i++) {
     console.log(btnsOpenMedal[i]);
-    btnsOpenMedal[i].addEventListener("click", function () {
-        modal.classList.remove("hidden");
-        overlay.classList.remove("hidden");
-    });
+    btnsOpenMedal[i].addEventListener("click", openMedal);
 }
-btnCloseMedal.addEventListener("click", CloseMedal);
-overlay.addEventListener("click", CloseMedal);
+btnCloseMedal.addEventListener("click", closeMedal);
+overlay.addEventListener("click", closeMedal);
